b3bb374b4ce844b142f2357c068dfedbcbb0c17a
abissi17 <galuxistar@gmail.com>
2026-05-26 13:39:27 -0700

    - fixed a file mispelling :/

--- pages/Overview.py
+++ pages/Overview.py
@@ -36,7 +36,7 @@ st.title("Feature Extraction from EEG Signals")
 st.text("""    Once we have segmented the EEG signals, we need to extract meaningful features that can be used for machine learning. Feature extraction is the process of transforming raw EEG data into a set of numerical features that capture important characteristics of the brain activity. We will extract features such as power spectral density, coherence, and entropy from the segmented EEG signals. These features can help us identify patterns in the brain activity that are associated with Alzheimer's disease. We will use these features to train our machine learning model and evaluate its performance in predicting Alzheimer's disease.
     """)
 st.image("pages/images/ADSpect.png", caption="Power Spectral Density feature extracted from EEG signals with AD")
-st.image("pages/images/HSpect.png", caption="Power Spectral Density feature extracted from EEG signals without AD")
+st.image("pages/images/Hspect.png", caption="Power Spectral Density feature extracted from EEG signals without AD")
 
 #========================================================================================
 st.title("Data Split: 70/12.5/12.5")
